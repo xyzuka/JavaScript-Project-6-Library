ff8e04893e5ea6f33df82e92caf5c6d01a51c0a9
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-03-08 16:52:02 +1100

    Display loop function
    Added a loop function to update the DOM based off the object containing all the default books

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
-// MODAL WINDOWS
+// MODAL WINDOW
+
 const show_modal = document.querySelectorAll(".add-book-button");
 const modal = document.querySelector(".modal");
 const close_modal = document.querySelector(".close-modal");
@@ -8,27 +9,22 @@ const overlay = document.querySelector(".overlay");
 
 const openModal = function () {
   modal.classList.remove("hidden");
-  overlay.classList.remove("hidden"); //adds background blur to modal pop up by removing the hidden class that hides the blur
+  overlay.classList.remove("hidden");
 };
 
-//Function that closes the modal
 const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
 
-//Loop to respond to a click event
 for (let i = 0; i < show_modal.length; i++) {
   show_modal[i].addEventListener("click", openModal);
 }
 
-//Close button functionality
 close_modal.addEventListener("click", closeModal);
 
-//Closes modal window when clicking outside
 overlay.addEventListener("click", closeModal);
 
-//ESC Keypress Event - using addEvenListener on the document as a global event
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape") {
     closeModal();
@@ -36,6 +32,7 @@ document.addEventListener("keydown", function (e) {
 });
 
 // DATA STRUCTURES AND ALGO
+
 const form = document.getElementById("form");
 const submit_button = document.querySelectorAll(".submit-book-button");
 const title = document.getElementById("Title").value;
@@ -51,6 +48,93 @@ class Book {
     this.read = read;
   }
 }
+
+// UI
+
+// Adds newly submitted book to display
+const updateBooksDisplay = () => {
+  for (let i = 0; i < myLibrary.length; i++) {
+    createBookDisplay(myLibrary[i]);
+  }
+};
+
+// Creates book DOM element when updating book display
+const createBookDisplay = (book) => {
+  // Creating a card (without styling)
+  const bookListContainer = document.querySelector(".book-list-container");
+  const bookContainer = document.createElement("div");
+  const bookTitleContainer = document.createElement("div");
+  const bookTitle = document.createElement("span");
+  const bookName = document.createElement("span");
+  const authorContainer = document.createElement("div");
+  const authorTitle = document.createElement("span");
+  const authorName = document.createElement("span");
+  const pagesContainer = document.createElement("div");
+  const pagesTitle = document.createElement("span");
+  const pages = document.createElement("span");
+  const buttonsContainer = document.createElement("div");
+  const readButton = document.createElement("button");
+  const removeButton = document.createElement("button");
+
+  // Adding styles to the card
+  bookContainer.classList.add("book-container-card");
+  bookTitle.textContent = "Book Title:";
+
+  bookTitleContainer.classList.add("book-title-container");
+
+  bookName.textContent = ` ${book.title}`;
+  bookTitle.classList.add("title");
+  bookName.classList.add("book-name");
+
+  authorContainer.classList.add("author-container");
+
+  authorTitle.textContent = "Author:";
+  authorName.textContent = ` ${book.author}`;
+  authorTitle.classList.add("title");
+  authorName.classList.add("title-name");
+
+  pagesContainer.classList.add("pages-container");
+
+  pagesTitle.textContent = "Pages:";
+  pages.textContent = ` ${book.pages}`;
+  pagesTitle.classList.add("title");
+  pages.classList.add("pages");
+
+  buttonsContainer.classList.add("buttons-container");  
+  // Read or unread button logic and CSS
+  if (book.read) {
+    readButton.classList.add("read-button-green");
+    readButton.classList.add("button");
+    readButton.textContent = 'Read';
+  } else {
+    readButton.classList.add("unread-button-red");
+    readButton.classList.add("button");
+    readButton.textContent = 'Unread';
+  }
+
+  // Remove button functionality
+  removeButton.classList.add("button");
+  removeButton.textContent = 'Remove';
+
+  // Appending newly created book spans
+  bookContainer.appendChild(bookTitleContainer);
+  bookTitleContainer.appendChild(bookTitle);
+  bookTitleContainer.appendChild(bookName);
+
+  bookContainer.appendChild(authorContainer);
+  authorContainer.appendChild(authorTitle);
+  authorContainer.appendChild(authorName);
+
+  bookContainer.appendChild(pagesContainer);
+  pagesContainer.appendChild(pagesTitle);
+  pagesContainer.appendChild(pages);
+
+  bookContainer.appendChild(buttonsContainer);
+  buttonsContainer.appendChild(readButton);
+  buttonsContainer.appendChild(removeButton);
+
+  bookListContainer.appendChild(bookContainer);
+};
 
 // Default book storage
 let myLibrary = [
@@ -81,6 +165,7 @@ const addBookToLibrary = (e) => {
 
   myLibrary.push(Book);
   closeModal();
+  //   updateBooksDisplay();
 };
 
 // Submit button click listener
@@ -90,4 +175,4 @@ form.addEventListener("submit", (e) => {
   form.reset(); // resets the form when submitted
 });
 
-// UI
+updateBooksDisplay();
